00e5208d1f12c6c17c8304db94a0e12aaa85beeb
mucahittuna <68152073+mucahittuna@users.noreply.github.com>
2021-02-07 19:29:18 +0300

    Tcp
    phone_and_C#_communicaton

---
+++ TcpV2/TcpV2/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TcpV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblconnect = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.txtRead = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblconnect
+            // 
+            this.lblconnect.AutoSize = true;
+            this.lblconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblconnect.Location = new System.Drawing.Point(214, 96);
+            this.lblconnect.Name = "lblconnect";
+            this.lblconnect.Size = new System.Drawing.Size(20, 29);
+            this.lblconnect.TabIndex = 10;
+            this.lblconnect.Text = ".";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(585, 480);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(143, 44);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(497, 219);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(231, 237);
+            this.txtSend.TabIndex = 8;
+            // 
+            // txtRead
+            // 
+            this.txtRead.Location = new System.Drawing.Point(197, 219);
+            this.txtRead.Multiline = true;
+            this.txtRead.Name = "txtRead";
+            this.txtRead.Size = new System.Drawing.Size(231, 237);
+            this.txtRead.TabIndex = 7;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(576, 96);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(143, 44);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 621);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblconnect);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtRead);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblconnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.TextBox txtRead;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TcpV2/TcpV2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace TcpV2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        TcpClient client = new TcpClient("192.168.1.105", 8080); // wifi IP adress,port
+
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            Thread thread;
+            thread = new Thread(new ThreadStart(read));
+            thread.Start();
+            lblconnect.Text = "Connected";
+            lblconnect.ForeColor = Color.Green;
+
+
+        }
+
+        private void read()
+        {
+            while (true)
+            {
+                Control.CheckForIllegalCrossThreadCalls = false;
+                NetworkStream stream = client.GetStream();
+                Byte[] data = new Byte[256];
+                String responseData = String.Empty;
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                txtRead.AppendText("Data : " + responseData + Environment.NewLine);
+
+            }
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string message;
+            message = txtSend.Text.ToString();
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+            NetworkStream stream = client.GetStream();
+            stream.Write(data, 0, data.Length);
+
+
+        }
+        int i = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;
+            label1.Text = i.ToString();
+        }
+    }
+}
