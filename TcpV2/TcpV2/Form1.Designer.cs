2615d5a340e68ff00fd137c033aed93b997fe46c
Burak Güzel <44478688+BurakYtu@users.noreply.github.com>
2021-02-11 20:27:57 +0300

    LiveChart Eklendi
    TCP read şuan için kapalı

--- TcpV2/TcpV2/Form1.Designer.cs
+++ TcpV2/TcpV2/Form1.Designer.cs
@@ -36,23 +36,30 @@
             this.btnConnect = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.cartesianChart1 = new LiveCharts.Wpf.CartesianChart();
+            this.livechart = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblconnect
             // 
             this.lblconnect.AutoSize = true;
             this.lblconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblconnect.Location = new System.Drawing.Point(214, 96);
+            this.lblconnect.Location = new System.Drawing.Point(160, 78);
+            this.lblconnect.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblconnect.Name = "lblconnect";
-            this.lblconnect.Size = new System.Drawing.Size(20, 29);
+            this.lblconnect.Size = new System.Drawing.Size(17, 25);
             this.lblconnect.TabIndex = 10;
             this.lblconnect.Text = ".";
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(585, 480);
+            this.btnSend.Location = new System.Drawing.Point(439, 390);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(143, 44);
+            this.btnSend.Size = new System.Drawing.Size(107, 36);
             this.btnSend.TabIndex = 9;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -60,25 +67,28 @@
             // 
             // txtSend
             // 
-            this.txtSend.Location = new System.Drawing.Point(497, 219);
+            this.txtSend.Location = new System.Drawing.Point(373, 178);
+            this.txtSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSend.Multiline = true;
             this.txtSend.Name = "txtSend";
-            this.txtSend.Size = new System.Drawing.Size(231, 237);
+            this.txtSend.Size = new System.Drawing.Size(174, 193);
             this.txtSend.TabIndex = 8;
             // 
             // txtRead
             // 
-            this.txtRead.Location = new System.Drawing.Point(197, 219);
+            this.txtRead.Location = new System.Drawing.Point(148, 178);
+            this.txtRead.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtRead.Multiline = true;
             this.txtRead.Name = "txtRead";
-            this.txtRead.Size = new System.Drawing.Size(231, 237);
+            this.txtRead.Size = new System.Drawing.Size(174, 193);
             this.txtRead.TabIndex = 7;
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(576, 96);
+            this.btnConnect.Location = new System.Drawing.Point(432, 78);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(143, 44);
+            this.btnConnect.Size = new System.Drawing.Size(107, 36);
             this.btnConnect.TabIndex = 6;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -87,9 +97,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 33);
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 11;
             this.label1.Text = "label1";
             // 
@@ -97,19 +108,46 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(620, 138);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(565, 202);
+            this.elementHost1.TabIndex = 12;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.cartesianChart1;
+            // 
+            // livechart
+            // 
+            this.livechart.Enabled = true;
+            this.livechart.Interval = 50;
+            this.livechart.Tick += new System.EventHandler(this.livechart_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(701, 380);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(484, 45);
+            this.trackBar1.TabIndex = 13;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(925, 621);
+            this.ClientSize = new System.Drawing.Size(1302, 495);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.elementHost1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblconnect);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.txtSend);
             this.Controls.Add(this.txtRead);
             this.Controls.Add(this.btnConnect);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,6 +162,10 @@
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private LiveCharts.Wpf.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Timer livechart;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- TcpV2/TcpV2/Form1.cs
+++ TcpV2/TcpV2/Form1.cs
@@ -11,18 +11,123 @@ using System.Net;
 using System.Net.Sockets;
 using System.IO;
 using System.Threading;
+using System.Windows.Media;
+
+
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Configurations;
+using Point = System.Windows.Point;
+using System.Drawing.Drawing2D;
+
+
 
 namespace TcpV2
 {
     public partial class Form1 : Form
     {
 
+        public class MeasureModel
+        {
+            public System.DateTime DateTime { get; set; }
+            public double Value { get; set; }
+        }
+
+        public ChartValues<MeasureModel> ChartValues { get; set; }
+
+        double minY = 0;
+        double maxY = 1000;
+        int data_from_tcp = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            #region Grandyan Çizgi Altı
+            var gradientBrush = new System.Windows.Media.LinearGradientBrush
+            {
+                StartPoint = new System.Windows.Point(0, 0),
+                EndPoint = new Point(0, 1)
+            };
+
+            gradientBrush.GradientStops.Add(new GradientStop(System.Windows.Media.Color.FromRgb(33, 148, 241), 0));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Transparent, 1));
+            #endregion
+
+            #region ile baglanti
+            var mapper = Mappers.Xy<MeasureModel>()
+               .X(model => model.DateTime.Ticks)   //use DateTime.Ticks as X
+               .Y(model => model.Value);           //use the value property as Y
+
+            Charting.For<MeasureModel>(mapper);
+            #endregion
+
+            #region Data Setini Ayarla
+            ChartValues = new ChartValues<MeasureModel>();
+
+            cartesianChart1.Series.Add(new LineSeries
+            {
+                Values = ChartValues,
+
+                Fill = gradientBrush,
+                StrokeThickness = 1,
+                PointGeometry = null
+            });
+            #endregion
+
+            #region Arka Plan Rengi
+            cartesianChart1.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 40, 50));
+            #endregion
+
+            #region Eksen Ayarları
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                //IsMerged = true,
+                //LabelFormatter = value => new System.DateTime((long)value).ToString("mm:ss"),
+                LabelFormatter = value => "",
+
+                Separator = new Separator
+                {
+                    //StrokeThickness = 1.5,
+                    //StrokeDashArray = new System.Windows.Media.DoubleCollection(new double[] { 4 }),
+                    Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(64, 79, 86)),
+                    Step = TimeSpan.FromMilliseconds(1000).Ticks
+                }
+
+            });
+
+            SetAxisLimits(System.DateTime.Now);
+
+            cartesianChart1.AxisY.Add(new Axis
+            {
+                
+                FontSize = 15,
+                MinValue = minY,
+                MaxValue = maxY,
+                LabelFormatter = val => val + " cm",
+                IsMerged = true,
+                Separator = new Separator
+                {
+                    StrokeThickness = 1.5,
+                    StrokeDashArray = new System.Windows.Media.DoubleCollection(new double[] { 4 }),
+                    Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(64, 79, 86))
+                }
+            });
+
+            cartesianChart1.DisableAnimations = true;
+            #endregion
+
+
         }
 
-        TcpClient client = new TcpClient("192.168.1.105", 8080); // wifi IP adress,port
+        private void SetAxisLimits(System.DateTime now)
+        {
+            //cartesianChart1.AxisX[0].MaxValue = now.Ticks + TimeSpan.FromSeconds(1).Ticks; // lets force the axis to be 100ms ahead
+            cartesianChart1.AxisX[0].MinValue = now.Ticks - TimeSpan.FromSeconds(10).Ticks; //we only care about the last 10 seconds
+        }
+
+
+        TcpClient client = new TcpClient("192.168.1.60", 8080); // wifi IP adress,port
 
 
         private void btnConnect_Click(object sender, EventArgs e)
@@ -32,13 +137,14 @@ namespace TcpV2
             thread = new Thread(new ThreadStart(read));
             thread.Start();
             lblconnect.Text = "Connected";
-            lblconnect.ForeColor = Color.Green;
+            lblconnect.ForeColor = System.Drawing.Color.Green;
 
 
         }
 
         private void read()
         {
+            /*
             while (true)
             {
                 Control.CheckForIllegalCrossThreadCalls = false;
@@ -49,8 +155,9 @@ namespace TcpV2
                 responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
                 txtRead.AppendText("Data : " + responseData + Environment.NewLine);
 
+                //data_from_tcp = Int32.Parse(responseData);
             }
-
+            */
         }
 
         private void btnSend_Click(object sender, EventArgs e)
@@ -68,6 +175,22 @@ namespace TcpV2
         {
             i++;
             label1.Text = i.ToString();
+        }
+
+        private void livechart_Tick(object sender, EventArgs e)
+        {
+            var now = System.DateTime.Now;
+
+            ChartValues.Add(new MeasureModel
+            {
+                DateTime = now,
+                Value = trackBar1.Value
+            }); ; ;
+
+            SetAxisLimits(now);
+
+            //lets only use the last 30 values
+            if (ChartValues.Count > 205) ChartValues.RemoveAt(0);
         }
     }
 }
